723d47fbb766594d4ba661532fb808be27a1ab36
karmvir.dhaliwal <karmvir.dhaliwal@cs.ucalgary.ca>
2021-12-13 06:22:27 +0000

    Final Update to MNIST_starter.py

--- MNIST_starter.py
+++ MNIST_starter.py
@@ -88,9 +88,9 @@ def prepData():
 
 trainingData, testingData = prepData()
 
-net = network.Network([784,10,10])
+net = network.Network([784,25,25,25,10])
 before = time.perf_counter()
-net.SGD(trainingData, 16, 10, 1, test_data = testingData)
+net.SGD(trainingData, 50, 5, 0.75, test_data = testingData)
 
 after = time.perf_counter()
 
